50dc98d901422129092c4d2eb76ed471e79b2450
Suegathh <suzzainnegathu@gmail.com>
2025-04-04 10:07:49 +0300

    register in admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Flight, FlightBooking
+
+admin.site.register(FlightBooking),
+admin.site.register(Flight)
--- hotels/admin.py
+++ hotels/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Hotel
 
-# Register your models here.
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'latitude', 'longitude', 'price_per_night', 'has_wifi', 'has_parking')
+    search_fields = ('name', 'address')
+    list_filter = ('has_wifi', 'has_parking')
+    ordering = ('price_per_night',)
+
+admin.site.register(Hotel, HotelAdmin)
